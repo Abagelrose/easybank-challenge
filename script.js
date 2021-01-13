b532e318f4587667e68fb87030fee43a0756cdfc
Abagelrose <76273345+Abagelrose@users.noreply.github.com>
2021-01-13 14:34:40 -0800

    Adding all new files

---
+++ script.js
@@ -0,0 +1,26 @@
+const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+
+btnHamburger.addEventListener('click', function(){
+    console.log('openhamburger');
+
+    if(header.classList.contains('open')){//CLose Hamburger menu
+        body.classList.remove('noscroll')
+        header.classList.remove('open');
+        fadeElems.forEach(function(element){
+            element.classList.add('fade-out');
+            element.classList.remove('fade-in');
+        });
+    }
+    else{//Open Hamburger menu
+        body.classList.add('noscroll')
+        header.classList.add('open');
+        fadeElems.forEach(function(element){
+            element.classList.add('fade-in');
+            element.classList.remove('fade-out');
+        });
+    }
+});
